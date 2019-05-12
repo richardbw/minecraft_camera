d2daa30557ce47404f53732ccced305b1070e8bd
BuildTools <unconfigured@null.spigotmc.org>
2019-05-12 08:00:00 +0100

    fix

--- pi/button_click.py
+++ pi/button_click.py
@@ -9,10 +9,15 @@ import os,configparser
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 import pygame 
 import logging,coloredlogs,sys,pprint,traceback 
+from picamera import PiCamera
 
 GPIO.setmode(GPIO.BCM) # use pin number printed on shield (ie no.s after GPIO at https://raspberrypi.stackexchange.com/a/12967))
 GPIO_BUTTON_PIN = 15
 GPIO_LED_PIN    = 14
+
+
+MP3_PROCESSING_FILENAME = os.path.dirname(os.path.abspath(__file__)) +'/replace_me.mp3'
+CAPTURED_PIC_FILENAME   = os.path.dirname(os.path.abspath(__file__)) +'/img/photobooth.jpg'
 
 # loggin config {{{
 logging.basicConfig(level=logging.DEBUG)
@@ -21,24 +26,50 @@ coloredlogs.install(level='DEBUG', logger=log)
 pp = pprint.PrettyPrinter(indent=4)  # USED?
 #}}}
 
+processing = False
+def button_callback(channel):
+    global processing
+    log.info("Button %s was pushed!"%(GPIO_BUTTON_PIN))
+    if processing:
+        log.warn("Still processing")
+        return
+
+    processing = True
+
+    log.info("Taking picture..")
+    burp = pygame.mixer.Sound(MP3_PROCESSING_FILENAME)
+    camera.capture(CAPTURED_PIC_FILENAME)
+
+    processing = False
+
 
 def main():
     log.info("Starting "+sys.argv[0])
-    GPIO.setup(GPIO_BUTTON_PIN, GPIO.IN,    pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(GPIO_LED_PIN,    GPIO.OUT,   initial=GPIO.LOW)
 
     try:  
-        while True: # Run forever
-            GPIO.output(GPIO_LED_PIN, GPIO.LOW)  # Turn LED off
-            if GPIO.input(GPIO_BUTTON_PIN) == GPIO.HIGH:
-                log.info("Button %s was pushed!"%(GPIO_BUTTON_PIN))
-                GPIO.output(GPIO_LED_PIN, GPIO.HIGH) # Turn LED on
+        camera =PiCamera()
+        camera.vflip = True 
+        camera.start_preview()
+
+        pygame.init() 
+        pygame.mixer.init() 
+
+        GPIO.setup(GPIO_BUTTON_PIN, GPIO.IN,    pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(GPIO_LED_PIN,    GPIO.OUT,   initial=GPIO.LOW)
+
+        # Setup event on rising edge
+        GPIO.add_event_detect(GPIO_BUTTON_PIN, GPIO.RISING, callback=button_callback) 
+
+        while True:
+            a=1
+            #:message = input("Press ENTER to quit: ")
       
     except KeyboardInterrupt:  
         log.warn("Caught ^C, and exiting")
     except:  
         log.exception("Unexpected exception occurred! ")
     finally:  
+        camera.stop_preview()
         log.debug("Cleaning up GPIO.")
         GPIO.cleanup() # this ensures a clean exit  
 
